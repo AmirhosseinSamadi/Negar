96ce6ec308197f6c31846a15f772fcfaf4916a93
Farid_mr <itmirengineering@gmail.com>
2024-01-02 11:53:47 +0330

    make sense 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,20 @@ module.exports = {
   content: ["./public/*.html", "./public/js/*.js"],
   theme: {
     extend: {},
-  },
-  container: {
-    center: true,
-    padding: "2rem"
-  },
-  screens: {
-    'vmini' : '390px',
-    'mobile' : '500px',
-    'tablet' : '768px',
-    'laptop' : '992px',
-    'desktop' : '1200px',
-    'large' : '1400px'
+    container: {
+      center: true,
+      padding: "2rem"
+    },
+    screens: {
+      'vmini' : '393px',
+      'mobile' : '500px',
+      'tablet' : '768px',
+      'laptop' : '992px',
+      'desktop' : '1200px',
+      'large' : '1400px'
+    }
   },
   plugins: [],
+  
 }
 
